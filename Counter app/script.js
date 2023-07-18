511fa6fc80f06f14c71151c3b24f4ade1ce05f69
Leman Mecidzade <lmanmcidzad@gmail.com>
2023-07-18 17:26:26 +0400

    finished

---
+++ Counter app/script.js
@@ -0,0 +1,18 @@
+const number = document.getElementById('count');
+const increment = document.getElementById('increment');
+const decrement = document.getElementById('decrement');
+
+let count = 0;
+
+
+increment.addEventListener('click', function() {
+  count++;
+  number.textContent = count;
+});
+
+
+decrement.addEventListener('click', function() {
+    count--;
+    number.textContent = count;
+});
+
